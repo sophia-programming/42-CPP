135d8b29de4fea9fd85eec64b181ae7ca04160c6
oaoba <hacchi0803.rera1126@gmail.com>
2024-03-24 03:21:45 +0900

    Makefile、.hpp作成

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,38 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat {
+private:
+	std::string name_;
+	int grade_;
+
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &rhs);
+	virtual ~Bureaucrat();
+	Bureaucrat &operator=(const Bureaucrat &rhs);
+
+	const std::string &getName() const;
+	void setName(const std::string &name);
+	int getGrade() const;
+	void setGrade(int grade);
+};
+
+/* colors */
+const char *const STOP = "\033[0m";
+const char *const BOLD = "\033[1m";
+const char *const BLACK = "\033[30m";
+const char *const RED = "\033[31m";
+const char *const GREEN = "\033[32m";
+const char *const YELLOW = "\033[33m";
+const char *const BLUE = "\033[34m";
+const char *const MAGENTA = "\033[35m";
+const char *const CYAN = "\033[36m";
+const char *const WHITE = "\033[37m";
+const char *const UNDERLINE =" \033[4m";
+
+#endif
