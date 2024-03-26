abf66c5dd9f1440242461b6582367d5d1de79f3a
oaoba <hacchi0803.rera1126@gmail.com>
2024-03-27 03:02:10 +0900

    [add] RobotomyRequestForm

---
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,55 @@
+#include "RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : Form("RobotomyRequestForm", 72, 45), target_("default") {
+	std::cout << CYAN << "[RobotomyRequestForm] Default constructor called" << STOP << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target) : Form("RobotomyRequestForm", 72, 45), target_(target) {
+	std::cout << CYAN << "[RobotomyRequestForm] Constructor called" << STOP << std::endl;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &rhs) : Form(rhs), target_(rhs.target_) {
+	std::cout << CYAN << "[RobotomyRequestForm] Copy constructor called" << STOP << std::endl;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm() {
+	std::cout << CYAN << "[RobotomyRequestForm] Destructor called" << STOP << std::endl;
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &rhs) {
+	std::cout << CYAN << "[RobotomyRequestForm] Assignation operator called" << STOP << std::endl;
+	if (this != &rhs) {
+		this->target_ = rhs.target_;
+	}
+	return *this;
+}
+
+void RobotomyRequestForm::execute(Bureaucrat const &executor) const {
+	if (executor.getGrade() > this->getGradeToExecute())
+		throw Form::GradeTooLowException();
+	else if (!this->getIsSigned())
+		throw Form::FormNotSignedException();
+	else {
+		std::cout << YELLOW << "Drilling noises" << STOP << std::endl;
+		srand(time(0));
+		if (rand() % 2){
+			std::cout << YELLOW << this->target_  << " has been robotomized successfully" << STOP << std::endl;
+			std::cout << "*************" << std::endl;
+			std::cout << rand() << std::endl;
+			std::cout << "*************" << std::endl;
+		}
+//
+		else
+			std::cout << RED << this->target_ <<  " robotomization failed" << STOP << std::endl;
+	}
+}
+
+std::ostream &operator<<(std::ostream &os, const RobotomyRequestForm &robotomyRequestForm) {
+	os << "RobotomyRequestForm " << robotomyRequestForm.getName() << " is ";
+	if (robotomyRequestForm.getIsSigned())
+		os << "signed";
+	else
+		os << "not signed";
+	os << " and requires grade " << robotomyRequestForm.getGradeToSign() << " to sign and grade " << robotomyRequestForm.getGradeToExecute() << " to execute";
+	return os;
+}
--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,11 +1,18 @@
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
+//gradeに大きな値（ex. 1000）を入れるとエラーが出る
 int main() {
 	ShrubberyCreationForm form("target");
-	Bureaucrat b("Bureaucrat", 150);
+	RobotomyRequestForm form2("Neko");
+	Bureaucrat b("Bureaucrat", 40);
 	try {
 		form.beSigned(b);
 		form.execute(b);
+		form2.beSigned(b);
+		form2.execute(b);
 	}
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
