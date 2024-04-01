20d693ce2552c595806681a7ecbc677cd19b434d
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-02 02:01:05 +0900

    役割ごとにディレクトリを分けて実装

---
+++ cpp06/ex02/includes/Base.hpp
@@ -0,0 +1,30 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <iostream>
+
+class Base
+{
+public:
+	virtual ~Base();
+};
+
+Base *generate(void);
+void identify(Base *p);
+void identify(Base &p);
+
+
+/* colors */
+const char *const STOP = "\033[0m";
+const char *const BOLD = "\033[1m";
+const char *const BLACK = "\033[30m";
+const char *const RED = "\033[31m";
+const char *const GREEN = "\033[32m";
+const char *const YELLOW = "\033[33m";
+const char *const BLUE = "\033[34m";
+const char *const MAGENTA = "\033[35m";
+const char *const CYAN = "\033[36m";
+const char *const WHITE = "\033[37m";
+const char *const UNDERLINE =" \033[4m";
+
+#endif
---
+++ cpp06/ex02/srcs/main.cpp
@@ -0,0 +1,47 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include "Base.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1 && argv)
+	{
+		std::cerr << "Invalid argument count!" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::cout << GREEN << "====================" << STOP << std::endl;
+	Base *a;
+	std::cout << "<TEST 1>" << std::endl;
+	a = generate();
+	identify(a);
+	identify(*a);
+
+	std::cout << GREEN << "====================" << STOP << std::endl;
+	Base *b;
+	std::cout << "<TEST 2>" << std::endl;
+	b = generate();
+	identify(b);
+	identify(*b);
+
+	std::cout << GREEN << "====================" << STOP << std::endl;
+	Base *c;
+	std::cout << "<TEST 3>" << std::endl;
+	c = generate();
+	identify(c);
+	identify(*c);
+
+	std::cout << RED << "====================" << STOP << std::endl;
+	Base *d = new Base;
+	std::cout << "<TEST 4>" << std::endl;
+	identify(d);
+	identify(*d);
+
+	delete a;;
+	delete b;
+	delete c;
+	delete d;
+
+	return EXIT_SUCCESS;
+}
