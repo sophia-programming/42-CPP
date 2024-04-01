b1113233f7121abe85a492a89a794bb626974864
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-01 20:10:28 +0900

    [learn] uintptr_t型

---
+++ cpp06/ex01/Serialization.hpp
@@ -0,0 +1,33 @@
+#ifndef SERIALIZATION_HPP
+#define SERIALIZATION_HPP
+
+#include <iostream>
+
+struct Data
+{
+	std::string s1;
+	int n;
+	std::string s2;
+};
+
+class Serialization
+{
+public:
+	static uintptr_t serialize(Data *ptr);
+	static Data *deserialize(uintptr_t raw);
+};
+
+/* colors */
+const char *const STOP = "\033[0m";
+const char *const BOLD = "\033[1m";
+const char *const BLACK = "\033[30m";
+const char *const RED = "\033[31m";
+const char *const GREEN = "\033[32m";
+const char *const YELLOW = "\033[33m";
+const char *const BLUE = "\033[34m";
+const char *const MAGENTA = "\033[35m";
+const char *const CYAN = "\033[36m";
+const char *const WHITE = "\033[37m";
+const char *const UNDERLINE =" \033[4m";
+
+#endif
---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,31 @@
+#include "Serialization.hpp"
+
+int main(void)
+{
+	Data *data = new Data;
+	std::cout << data << std::endl;
+	data->s1 = "Hello";
+	data->n = 42;
+	data->s2 = "Neko";
+
+	uintptr_t row = Serialization::serialize(data);
+	std::cout << YELLOW << "serialize address: " << STOP << row << std::endl;
+
+	Data *deserialized = Serialization::deserialize(row);
+	std::cout << YELLOW << "deserialized pointer address:  " << STOP << deserialized << std::endl;
+
+	std::cout << CYAN << "Original pointer address: " << STOP << data << std::endl;
+	std::cout << "s1: " << deserialized->s1 << std::endl;
+	std::cout << "n: " << deserialized->n << std::endl;
+	std::cout << "s2: " << deserialized->s2 << std::endl;
+
+	std::cout << "---------------------------" << std::endl;
+
+	std::cout << CYAN << "Deserialized pointer address: " << STOP << deserialized << std::endl;
+	std::cout << "s1: " << data->s1 << std::endl;
+	std::cout << "n: " << data->n << std::endl;
+	std::cout << "s2: " << data->s2 << std::endl;
+
+	delete data;
+	return 0;
+}
