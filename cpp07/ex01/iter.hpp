c64c2b9940910074ca040051711b1ef74031278b
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-02 16:25:52 +0900

    main.cppを適応できるようにする必要がある

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,31 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template <typename T>
+void iter(T *array, size_t length, void (*f)(T const &)) {
+	for (size_t i = 0; i < length; i++) {
+		f(array[i]);
+	}
+}
+
+template <typename T>
+void print(T const &x) {
+	std::cout << x << std::endl;
+}
+
+/* colors */
+const char *const STOP = "\033[0m";
+const char *const BOLD = "\033[1m";
+const char *const BLACK = "\033[30m";
+const char *const RED = "\033[31m";
+const char *const GREEN = "\033[32m";
+const char *const YELLOW = "\033[33m";
+const char *const BLUE = "\033[34m";
+const char *const MAGENTA = "\033[35m";
+const char *const CYAN = "\033[36m";
+const char *const WHITE = "\033[37m";
+const char *const UNDERLINE =" \033[4m";
+
+#endif
