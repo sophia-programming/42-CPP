999ea71c248e576a6a1589bbd80f4ecbd558676c
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-16 20:20:37 +0900

    [fix] main関数を修正

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -10,39 +10,31 @@ int main() {
 	Bureaucrat lowRankBureaucrat("LowRank", 150);
 
 	try {
-		lowRankBureaucrat.signForm(form);
-		lowRankBureaucrat.executeForm(form);
+		highRankBureaucrat.signForm(form);
+		highRankBureaucrat.executeForm(form);
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "==========================" << std::endl;
+	RobotomyRequestForm form1("neko");
+
+	try {
+		lowRankBureaucrat.signForm(form1);
+		lowRankBureaucrat.executeForm(form1);
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << "==========================" << std::endl;
+	PresidentialPardonForm form2("dog");
+
+	try {
+		highRankBureaucrat.signForm(form2);
+		highRankBureaucrat.executeForm(form2);
 	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
 	return 0;
 }
-//
-//int main() {
-//	RobotomyRequestForm form("neko");
-//	Bureaucrat highRankBureaucrat("HighRank", 1);
-//	Bureaucrat lowRankBureaucrat("LowRank", 70);
-//
-//	try {
-//		lowRankBureaucrat.signForm(form);
-//		lowRankBureaucrat.executeForm(form);
-//	} catch (std::exception& e) {
-//		std::cout << e.what() << std::endl;
-//	}
-//	return 0;
-//}
-//
-//int main() {
-//	PresidentialPardonForm form("dog");
-//	Bureaucrat highRankBureaucrat("HighRank", 1);
-//	Bureaucrat lowRankBureaucrat("LowRank", 150);
-//
-//	try {
-//		highRankBureaucrat.signForm(form);
-//		highRankBureaucrat.executeForm(form);
-//	} catch (std::exception& e) {
-//		std::cout << e.what() << std::endl;
-//	}
-//	return 0;
-//}
-//
+
