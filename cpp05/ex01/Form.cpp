9cc2b60673a6c33d2c541266431a1e3b43e6f00d
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-16 20:08:16 +0900

    [add] Form classのエラー処理追加

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,85 +1,78 @@
 #include "Form.hpp"
 
-Form::Form() : name_("Default"), gradeToSign_(5), gradeToExecute_(6)
-{
+Form::Form() : name_("Default"), gradeToSign_(5), gradeToExecute_(6){
 	isSigned_ = false;
 	std::cout << "[Form] Default constructor called" << std::endl;
+	if (this->gradeToSign_ < 1)
+		throw Form::GradeTooHighException();
+	else if (this->gradeToSign_ > 150)
+		throw Form::GradeTooLowException();
 }
 
-Form::Form(const std::string &name, const int gradeToSign, const int gradeToExecute) : name_(name), gradeToSign_(gradeToSign), gradeToExecute_(gradeToExecute)
-{
+Form::Form(const std::string &name, const int gradeToSign, const int gradeToExecute) : name_(name), gradeToSign_(gradeToSign), gradeToExecute_(gradeToExecute){
 	isSigned_ = false;
 	std::cout << "[Form] Constructor called" << std::endl;
+	if (this->gradeToSign_ < 1)
+		throw Form::GradeTooHighException();
+	else if (this->gradeToSign_ > 150)
+		throw Form::GradeTooLowException();
 }
 
-Form::Form(const Form &rhs) : name_(rhs.name_), gradeToSign_(rhs.gradeToSign_), gradeToExecute_(rhs.gradeToExecute_)
-{
+Form::Form(const Form &rhs) : name_(rhs.name_), gradeToSign_(rhs.gradeToSign_), gradeToExecute_(rhs.gradeToExecute_){
 	std::cout << "[Form] Copy constructor called" << std::endl;
 }
 
-Form::~Form()
-{
+Form::~Form(){
 	std::cout << "[Form] Destructor called" << std::endl;
 }
 
-Form &Form::operator=(const Form &rhs)
-{
+Form &Form::operator=(const Form &rhs){
 	std::cout << "[Form] Assignation operator called" << std::endl;
-	if (this != &rhs)
-	{
+	if (this != &rhs)	{
 		this->isSigned_ = rhs.getIsSigned();
 	}
 	return *this;
 }
 
-std::string Form::getName() const
-{
+std::string Form::getName() const{
 	return this->name_;
 }
 
-bool Form::getIsSigned() const
-{
+bool Form::getIsSigned() const{
 	return this->isSigned_;
 }
 
-int Form::getGradeToSign() const
-{
+int Form::getGradeToSign() const{
 	return this->gradeToSign_;
 }
 
-int Form::getGradeToExecute() const
-{
+int Form::getGradeToExecute() const{
 	return this->gradeToExecute_;
 }
 
-void Form::beSigned(const Bureaucrat &bureaucrat)
-{
+void Form::beSigned(const Bureaucrat &bureaucrat){
 	if (bureaucrat.getGrade() <= this->gradeToSign_)
 		this->isSigned_ = true;
 	else
 		throw Form::GradeTooLowException();
 }
 
-void Form::execute(const Bureaucrat &executor)
-{
+void Form::execute(const Bureaucrat &executor){
 	if (executor.getGrade() <= this->gradeToExecute_)
 		this->isSigned_ = true;
 	else
 		throw Form::GradeTooLowException();
 }
 
-const char *Form::GradeTooHighException::what() const throw()
-{
+const char *Form::GradeTooHighException::what() const throw(){
 	return "Grade is too high";
 }
 
-const char *Form::GradeTooLowException::what() const throw()
-{
+const char *Form::GradeTooLowException::what() const throw(){
 	return "Grade is too low";
 }
 
-std::ostream& operator<<(std::ostream &os, const Form &form)
-{
+std::ostream& operator<<(std::ostream &os, const Form &form){
 	os << "Form name: " << form.getName() << std::endl;
 	os << "Grade to sign: " << form.getGradeToSign() << std::endl;
 	os << "Grade to execute: " << form.getGradeToExecute() << std::endl;
