1e273d1e3dbd33297aaed4532fe7d6bbcf5d0a92
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-16 19:45:42 +0900

    [fix] nameにconstをつけた（課題要件）

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -8,7 +8,44 @@ int main(void)
 		try
 		{
 			b.decrementGrade();
-//			b.incrementGrade();
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << b << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << RED << "Grade out of range " << e.what() << STOP << std::endl;
+		return (0);
+	}
+	std::cout << "==========================" << std::endl;
+	try
+	{
+		Bureaucrat b("Bureaucrat", 150);
+		try
+		{
+			b.incrementGrade();
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		std::cout << b << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << RED << "Grade out of range " << e.what() << STOP << std::endl;
+		return (0);
+	}
+	std::cout << "==========================" << std::endl;
+	try
+	{
+		Bureaucrat b("Bureaucrat", 1111);
+		try
+		{
+			b.incrementGrade();
 		}
 		catch (std::exception & e)
 		{
