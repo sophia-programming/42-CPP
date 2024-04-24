668d1a30e1076261ea114eae438d8d5ade88a506
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-25 01:10:03 +0900

    [update] test of list

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,6 +1,7 @@
 #include "MutantStack.hpp"
+#include "list"
 
-int main()
+void pdf_test()
 {
 	MutantStack<int> ms;
 	std::cout << GREEN << "=== [top] ===" << STOP << std::endl;
@@ -28,5 +29,45 @@ int main()
 		it++;
 	}
 	std::stack<int> s(ms);
+}
+
+void my_test()
+{
+	std::cout << GREEN << "=== [MutantStack with int] ===" << STOP << std::endl;
+	MutantStack<int> ms1;
+	ms1.push(5);
+	ms1.push(17);
+	ms1.pop();
+	ms1.push(-35);
+	ms1.push(10);
+	for (MutantStack<int>::iterator it = ms1.begin(); it != ms1.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	std::cout << GREEN << "=== [MutantStack with list] ===" << STOP << std::endl;
+	MutantStack<std::list<int> > ms2;
+	std::list<int> list1;
+	list1.push_back(5);
+	list1.push_back(17);
+	ms2.push(list1); //リスト全体をMutantStackに追加
+
+	std::list<int> list2;
+	list2.push_back(-35);
+	list2.push_back(10);
+	ms2.push(list2); //別リストをMutantStackに追加
+
+	for (MutantStack<std::list<int> >::iterator it = ms2.begin(); it != ms2.end(); it++)
+	{
+		for (std::list<int>::iterator itList = it->begin(); itList != it->end(); itList++)
+		{
+			std::cout << *itList << std::endl;
+		}
+	}
+}
+
+int main() {
+	pdf_test();
+	my_test();
 	return 0;
 }
