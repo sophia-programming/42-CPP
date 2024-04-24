6da77b19feb5df7d8b69670bb900f9246ac8ae46
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-25 01:52:31 +0900

    [add] test of double

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -35,34 +35,60 @@ void my_test()
 {
 	std::cout << GREEN << "=== [MutantStack with int] ===" << STOP << std::endl;
 	MutantStack<int> ms1;
+	std::cout << "empty(): " << ms1.empty() << std::endl;
 	ms1.push(5);
 	ms1.push(17);
 	ms1.pop();
 	ms1.push(-35);
 	ms1.push(10);
+	std::cout << "add: " << ms1.empty() << std::endl;
 	for (MutantStack<int>::iterator it = ms1.begin(); it != ms1.end(); it++)
 	{
 		std::cout << *it << std::endl;
 	}
 
+	std::cout << GREEN << "=== [MutantStack with double] ===" << STOP << std::endl;
+	MutantStack<double> ms2;
+	ms2.push(5.5);
+	ms2.push(17.5);
+	ms2.pop();
+	ms2.push(-35.5);
+	ms2.push(10.5);
+	for (MutantStack<double>::iterator it = ms2.begin(); it != ms2.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
 	std::cout << GREEN << "=== [MutantStack with list] ===" << STOP << std::endl;
-	MutantStack<std::list<int> > ms2;
+	MutantStack<std::list<int> > ms3;
 	std::list<int> list1;
 	list1.push_back(5);
 	list1.push_back(17);
-	ms2.push(list1); //リスト全体をMutantStackに追加
+	ms3.push(list1); //リスト全体をMutantStackに追加
 
 	std::list<int> list2;
 	list2.push_back(-35);
 	list2.push_back(10);
-	ms2.push(list2); //別リストをMutantStackに追加
+	ms3.push(list2); //別リストをMutantStackに追加
 
-	for (MutantStack<std::list<int> >::iterator it = ms2.begin(); it != ms2.end(); it++)
+	for (MutantStack<std::list<int> >::iterator it = ms3.begin(); it != ms3.end(); it++)
 	{
 		for (std::list<int>::iterator itList = it->begin(); itList != it->end(); itList++)
 		{
 			std::cout << *itList << std::endl;
 		}
+	}
+
+	std::cout << GREEN << "=== [MutantStack with string] ===" << STOP << std::endl;
+	MutantStack<std::string> ms4;
+	std::string str;
+	ms4.push("Hello");
+	ms4.push("Neko");
+	ms4.push("!");
+
+	for (MutantStack<std::string>::iterator it = ms4.begin(); it != ms4.end(); it++)
+	{
+		std::cout << *it << std::endl;
 	}
 }
 
