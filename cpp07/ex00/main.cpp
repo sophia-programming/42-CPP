289ee8b42a34ece7b15fdf3c53a363ad0564d2ed
oaoba <hacchi0803.rera1126@gmail.com>
2024-04-24 14:11:25 +0900

    [add] test_case

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,6 +1,7 @@
 #include "whatever.hpp"
 
-int main( void ) {
+static int assignment_test(void){
+	std::cout << GREEN << "======= assignment_test =======" << STOP << std::endl;
 	int a = 2;
 	int b = 3;
 	::swap( a, b );
@@ -15,5 +16,39 @@ int main( void ) {
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
+
+static int	my_test(void) {
+	std::cout << GREEN << "========== my_test ==========" << STOP << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine";
+
+	std::cout << YELLOW << "Before swap" << STOP << std::endl;
+	std::cout << "c[" << &c << "] = " << c << ", d[" << &d << "] = " << d << std::endl;
+	::swap(c, d);
+	std::cout << YELLOW << "After swap" << STOP << std::endl;
+	std::cout << "c[" << &c << "] = " << c << ", d[" << &d << "] = " << d << std::endl;
+
+	std::cout  << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout  << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	std::cout << GREEN << "Different types" << STOP << std::endl;
+	float	a = 3.4;
+	int		b = 2;
+
+	std::cout << YELLOW << "Before swap" << STOP << std::endl;
+	std::cout << "a[" << &a << "] = " << a << ", b[" << &b << "] = " << b << std::endl;
+	::swap( a, b );
+	std::cout << YELLOW << "After swap" << STOP << std::endl;
+	std::cout << "a[" << &a << "] = " << a << ", b[" << &b << "] = " << b << std::endl;
+	std::cout  << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout  << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	return (0);
+}
+
+int main() {
+	assignment_test();
+	my_test();
 	return 0;
 }
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -11,16 +11,33 @@ void swap(T &a, T &b) {
 }
 
 template< typename T >
-T min(T a, T b) {
+T min(T &a, T &b) {
 	if (a == b)
 		return b;
-	return (a > b ? b : a);
+	return (a < b ? a : b);
 }
 
 template< typename T >
-T max(T a, T b) {
+T max(T &a, T &b) {
 	if (a == b)
 		return b;
+	return (a > b ? a : b);
+}
+
+template <typename T1, typename T2>
+void swap(T1 &a, T2 &b) {
+	T1 tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T1, typename T2>
+T1	min(T1 &a, T2 &b) {
+	return (a < b ? a : b);
+}
+
+template <typename T1, typename T2>
+T1	max(T1 &a, T2 &b) {
 	return (a > b ? a : b);
 }
 
